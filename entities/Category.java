6cb962850aaf445f47578167285190e602e283bb
D1_Prathamesh_Lawand_83746 <lawandprathamesh11@gmail.com>
2024-08-06 16:01:51 +0530

    updatedEntities

--- entities/Category.java
+++ entities/Category.java
@@ -1,16 +1,25 @@
 package com.app.entities;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
 @Entity
 public class Category extends BaseEntity{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long categoryId;
 	
 	@Column(length = 15)
 	private CategoryEnum categoryName;
